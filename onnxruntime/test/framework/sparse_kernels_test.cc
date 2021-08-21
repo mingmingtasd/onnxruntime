91190f7b618cd72eaa684b09fdf146f80e9e6882
Dmitri Smirnov <dmitrism@microsoft.com>
2021-08-20 18:07:36 -0700

    Fix graph test

--- onnxruntime/test/framework/sparse_kernels_test.cc
+++ onnxruntime/test/framework/sparse_kernels_test.cc
@@ -1045,59 +1045,6 @@ int64_t ActualSize(const SparseTensorProto& actual) {
   return actual_size;
 }
 
-void IndicesChecker(const TensorProto& indices_proto, gsl::span<const int64_t> expected_indicies) {
-  gsl::span<const int64_t> ind_span;
-  std::vector<int64_t> converted_indices;
-  TensorShape ind_shape(indices_proto.dims().data(), indices_proto.dims().size());
-  const auto elements = gsl::narrow<size_t>(ind_shape.Size());
-  const bool has_raw_data = indices_proto.has_raw_data();
-  switch (indices_proto.data_type()) {
-    case ONNX_NAMESPACE::TensorProto_DataType_INT64: {
-      if (has_raw_data) {
-        const auto& rd = indices_proto.raw_data();
-        ASSERT_EQ(rd.size(), elements * sizeof(int64_t));
-        ind_span = gsl::make_span(reinterpret_cast<const int64_t*>(rd.data()), elements);
-      } else {
-        ind_span = gsl::make_span(indices_proto.int64_data().cbegin(), indices_proto.int64_data().cend());
-      }
-      break;
-    }
-    case ONNX_NAMESPACE::TensorProto_DataType_INT32: {
-      if (has_raw_data) {
-        const auto& rd = indices_proto.raw_data();
-        ASSERT_EQ(rd.size(), elements * sizeof(int32_t));
-        auto int32_span =  gsl::make_span(reinterpret_cast<const int32_t*>(rd.data()), elements);
-        converted_indices.insert(converted_indices.cend(), int32_span.cbegin(), int32_span.cend());
-      } else {
-        converted_indices.insert(converted_indices.cend(), indices_proto.int32_data().cbegin(), indices_proto.int32_data().cend());
-      }
-      ind_span = gsl::make_span(converted_indices);
-      break;
-    }
-    case ONNX_NAMESPACE::TensorProto_DataType_INT16: {
-      ASSERT_TRUE(has_raw_data);
-      const auto& rd = indices_proto.raw_data();
-      ASSERT_EQ(rd.size(), elements * sizeof(int16_t));
-      auto int16_span = gsl::make_span(reinterpret_cast<const int16_t*>(rd.data()), elements);
-      converted_indices.insert(converted_indices.cend(), int16_span.cbegin(), int16_span.cend());
-      ind_span = gsl::make_span(converted_indices);
-      break;
-    }
-    case ONNX_NAMESPACE::TensorProto_DataType_INT8: {
-      ASSERT_TRUE(has_raw_data);
-      const auto& rd = indices_proto.raw_data();
-      ASSERT_EQ(rd.size(), elements);
-      auto int8_span = gsl::make_span(reinterpret_cast<const int8_t*>(rd.data()), elements);
-      converted_indices.insert(converted_indices.cend(), int8_span.cbegin(), int8_span.cend());
-      ind_span = gsl::make_span(converted_indices);
-      break;
-    }
-    default:
-      ASSERT_TRUE(false);
-  }
-  ASSERT_THAT(ind_span, testing::ContainerEq(expected_indicies));
-}
-
 template <typename T>
 static void RawSparseDataChecker(gsl::span<const T> expected_values,
                                  gsl::span<const int64_t> expected_indicies,
@@ -1109,7 +1056,7 @@ static void RawSparseDataChecker(gsl::span<const T> expected_values,
 
   ASSERT_THAT(actual_span, testing::ContainerEq(expected_values));
 
-  IndicesChecker(actual.indices(), expected_indicies);
+  SparseIndicesChecker(actual.indices(), expected_indicies);
 }
 
 template <>
@@ -1124,7 +1071,7 @@ void RawSparseDataChecker<BFloat16>(gsl::span<const BFloat16> expected_bfloat,
   auto actual_span = gsl::make_span<const uint16_t>(raw_data, actual_size);
 
   ASSERT_THAT(actual_span, testing::ContainerEq(expected));
-  IndicesChecker(actual.indices(), expected_indicies);
+  SparseIndicesChecker(actual.indices(), expected_indicies);
 }
 
 template <>
@@ -1139,7 +1086,7 @@ void RawSparseDataChecker<MLFloat16>(gsl::span<const MLFloat16> expected_bfloat,
   auto actual_span = gsl::make_span<const uint16_t>(raw_data, actual_size);
 
   ASSERT_THAT(actual_span, testing::ContainerEq(expected));
-  IndicesChecker(actual.indices(), expected_indicies);
+  SparseIndicesChecker(actual.indices(), expected_indicies);
 }
 
 template <typename T>
